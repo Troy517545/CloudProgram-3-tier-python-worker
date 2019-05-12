f737c2f8718e4f6b70452aa6e850f0c590669132
105060005 <troy517545@gmail.com>
2019-05-13 02:16:52 +0800

    v8

--- application.py
+++ application.py
@@ -2,6 +2,7 @@ import logging
 
 logging.basicConfig(filename='example.log',level=logging.DEBUG)
 logging.info('python worker app start')
+logging.info('hi')
 
 import flask
 from flask import request, Response
@@ -13,6 +14,25 @@ import boto
 
 # s3_output_bucket = "nthu-105060005"
 # write_excel_to_s3('example.log','example.log', s3_output_bucket)
+
+application = flask.Flask(__name__)
+
+
+def write_excel_to_s3(path, file_name, s3_output_bucket):
+    # Connect to S3 and get the output bucket
+    #s3 = boto.connect_s3(host=s3_endpoint)
+    s3 = boto.connect_s3()
+    output_bucket = s3.get_bucket(s3_output_bucket)
+
+    # Create a key to store the instances_json text
+    k = Key(output_bucket)
+    k.key = file_name
+    #k.set_metadata("Content-Type", "image/jpeg")
+    k.set_contents_from_filename(path)
+    k.set_acl('public-read')
+
+    # Return a URL to the object
+    return "https://%s.s3.amazonaws.com/%s" % (s3_output_bucket, k.key)
 
 
 @application.route('/worker', methods=['POST'])
@@ -37,26 +57,9 @@ def worker():
     return response
     
 
-
-
-def write_excel_to_s3(path, file_name, s3_output_bucket):
-    # Connect to S3 and get the output bucket
-    #s3 = boto.connect_s3(host=s3_endpoint)
-    s3 = boto.connect_s3()
-    output_bucket = s3.get_bucket(s3_output_bucket)
-
-    # Create a key to store the instances_json text
-    k = Key(output_bucket)
-    k.key = file_name
-    #k.set_metadata("Content-Type", "image/jpeg")
-    k.set_contents_from_filename(path)
-    k.set_acl('public-read')
-
-    # Return a URL to the object
-    return "https://%s.s3.amazonaws.com/%s" % (s3_output_bucket, k.key)
-
+# test
 
 
 if __name__ == '__main__':
     #excel.init_excel(application)
-    application.run(host='0.0.0.0', port='50')
+    application.run(host='127.0.0.1', port='80')
